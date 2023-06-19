160289bb249fd8634e26c350053ccaa8c101b39a
Yumu0112 <yumu0112@icloud.com>
2023-06-19 11:56:37 +0900

    JSON response

--- src/main/java/com/example/demo/CurrentTimeController.java
+++ src/main/java/com/example/demo/CurrentTimeController.java
@@ -7,7 +7,31 @@ import java.time.LocalDateTime;
 @RestController
 public class CurrentTimeController {
         @GetMapping("/current")
-        public String getTime() {
-            return "日時：" +  LocalDateTime.now();
+        public CurrentTimeResponse getTime() {
+
+//            return "日時：" +  LocalDateTime.now();
+
+        // 現在時刻を格納する変数を定義
+           LocalDateTime currentTime = LocalDateTime.now();
+           return new CurrentTimeResponse(currentTime);
+        }
+
+        // 内部クラスで時刻を保持、JSONオブジェクトの生成
+        public static class CurrentTimeResponse {
+            private LocalDateTime timestamp;
+
+
+            public CurrentTimeResponse(LocalDateTime currentTime) {
+                this.timestamp = currentTime;
+            }
+
+        // getterとsetter: 外部からアクセスできるようにする
+            public LocalDateTime getTimestamp() {
+                return timestamp;
+            }
+
+            public void setTimestamp(LocalDateTime timestamp) {
+                this.timestamp = timestamp;
+            }
         }
 }
